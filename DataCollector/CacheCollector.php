d62afc21f49cacc702803158d4591a69847ed4be
Georges.L <contact@geolim4.com>
2016-09-18 18:37:18 +0200

    Added API changelog to colelctor + Added field description on hover
    (cherry picked from commit 1626390fa89aa5dfe4b6495392776853974129e3)

--- DataCollector/CacheCollector.php
+++ DataCollector/CacheCollector.php
@@ -1,17 +1,4 @@
 <?php
-/**
- *
- * This file is part of phpFastCache.
- *
- * @license MIT License (MIT)
- *
- * For full copyright and license information, please see the docs/CREDITS.txt file.
- *
- * @author Georges.L (Geolim4)  <contact@geolim4.com>
- * @author PastisD https://github.com/PastisD
- * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> http://www.phpfastcache.com
- *
- */
 
 namespace phpFastCache\Bundle\DataCollector;
 
@@ -67,6 +54,7 @@ class CacheCollector extends DataCollector
 
         $this->data = [
           'apiVersion' => phpFastCacheApi::getVersion(),
+          'apiChangelog' => phpFastCacheApi::getChangelog(),
           'driverUsed' => $driverUsed,
           'instances' => $instances,
           'stats' => $stats,
@@ -137,6 +125,13 @@ class CacheCollector extends DataCollector
         return $this->data[ 'apiVersion' ];
     }
 
+    /**
+     * @return mixed
+     */
+    public function getApiChangelog()
+    {
+        return $this->data[ 'apiChangelog' ];
+    }
 
     /**
      * @return string
