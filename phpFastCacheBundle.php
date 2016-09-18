9b35be6e77d4e9c3201bfa77cfbb25fe986d21e0
Georges.L <contact@geolim4.com>
2016-09-18 16:43:25 +0200

    Added legal headers
    (cherry picked from commit f643400cc40b73f9d598c87922485e20ebe95d9b)

--- phpFastCacheBundle.php
+++ phpFastCacheBundle.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ *
+ * This file is part of phpFastCache.
+ *
+ * @license MIT License (MIT)
+ *
+ * For full copyright and license information, please see the docs/CREDITS.txt file.
+ *
+ * @author Georges.L (Geolim4)  <contact@geolim4.com>
+ * @author PastisD https://github.com/PastisD
+ * @author Khoa Bui (khoaofgod)  <khoaofgod@gmail.com> http://www.phpfastcache.com
+ *
+ */
 
 namespace phpFastCache\Bundle;
 
