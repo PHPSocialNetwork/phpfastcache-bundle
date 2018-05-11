3fbecc8ed05838009a6537d2b263247ada78f767
Georges.L <contact@geolim4.com>
2018-05-11 18:57:45 +0200

    Preparing tests...

---
+++ tests/Service/PhpfastcacheTest.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the SncRedisBundle package.
+ *
+ * (c) Henrik Westphal <henrik.westphal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Phpfastcache\PhpfastcacheBundle\Tests\Service;
+use PHPUnit\Framework\TestCase;
+
+class PhpfastcacheTest extends TestCase
+{
+
+}
